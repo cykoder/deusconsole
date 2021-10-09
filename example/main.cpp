d955abfa25e813fb7af9296b1715a14e9bcf1ca9
Samuel Hellawell <sshellawell@gmail.com>
2021-10-09 16:12:07 +0100

    style example

--- example/main.cpp
+++ example/main.cpp
@@ -20,6 +20,7 @@
 // Having a single function that acts as a loop prevents us to store state in the stack of said function. So we need some location for this.
 SDL_Window*     g_Window = NULL;
 SDL_GLContext   g_GLContext = NULL;
+inline void setGUIStyles();
 
 // We will also declare some demo variables globally so we can keep this program procedural for simplification purposes
 // in your real implementation of deus console you will probably want to wrap into a class
@@ -327,6 +328,7 @@ int main(int, char**) {
   // Setup Dear ImGui style
   ImGui::StyleColorsDark();
   //ImGui::StyleColorsClassic();
+  setGUIStyles();
 
   // Setup Platform/Renderer backends
   ImGui_ImplSDL2_InitForOpenGL(g_Window, g_GLContext);
@@ -340,4 +342,88 @@ int main(int, char**) {
     main_loop(nullptr);
   }
   #endif
+}
+
+// Misc styling
+inline void setGUIStyles() {
+	ImGuiStyle* style = &ImGui::GetStyle();
+	ImVec4* colors = style->Colors;
+
+  colors[ImGuiCol_Text]                   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+  colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+  colors[ImGuiCol_WindowBg]               = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+  colors[ImGuiCol_ChildBg]                = ImVec4(0.28f, 0.28f, 0.28f, 0.00f);
+  colors[ImGuiCol_PopupBg]                = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+  colors[ImGuiCol_Border]                 = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+  colors[ImGuiCol_BorderShadow]           = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_FrameBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.13f);
+  colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+  colors[ImGuiCol_FrameBgActive]          = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+  colors[ImGuiCol_TitleBg]                = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+  colors[ImGuiCol_TitleBgActive]          = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+  colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+  colors[ImGuiCol_MenuBarBg]              = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+  colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.16f, 0.16f, 0.16f, 0.00f);
+  colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.30f, 0.30f, 0.30f, 1.00f);
+  colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_CheckMark]              = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_SliderGrab]             = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+  colors[ImGuiCol_SliderGrabActive]       = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_Button]                 = ImVec4(1.00f, 1.00f, 1.00f, 0.00f);
+  colors[ImGuiCol_ButtonHovered]          = ImVec4(1.00f, 0.37f, 0.17f, 0.25f);
+  colors[ImGuiCol_ButtonActive]           = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_Header]                 = ImVec4(0.75f, 0.75f, 0.75f, 0.25f);
+  colors[ImGuiCol_HeaderHovered]          = ImVec4(1.00f, 0.37f, 0.17f, 0.25f);
+  colors[ImGuiCol_HeaderActive]           = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_Separator]              = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+  colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+  colors[ImGuiCol_SeparatorActive]        = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_ResizeGrip]             = ImVec4(1.00f, 1.00f, 1.00f, 0.25f);
+  colors[ImGuiCol_ResizeGripHovered]      = ImVec4(1.00f, 1.00f, 1.00f, 0.67f);
+  colors[ImGuiCol_ResizeGripActive]       = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_Tab]                    = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+  colors[ImGuiCol_TabHovered]             = ImVec4(0.24f, 0.24f, 0.24f, 1.00f);
+  colors[ImGuiCol_TabActive]              = ImVec4(0.24f, 0.24f, 0.24f, 1.00f);
+  colors[ImGuiCol_TabUnfocused]           = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+  colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.24f, 0.24f, 0.24f, 1.00f);
+  colors[ImGuiCol_PlotLines]              = ImVec4(1.00f, 0.37f, 0.17f, 1.00f);
+  colors[ImGuiCol_PlotLinesHovered]       = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_PlotHistogram]          = ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
+  colors[ImGuiCol_PlotHistogramHovered]   = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_TableHeaderBg]          = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
+  colors[ImGuiCol_TableBorderStrong]      = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
+  colors[ImGuiCol_TableBorderLight]       = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
+  colors[ImGuiCol_TableRowBg]             = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+  colors[ImGuiCol_TableRowBgAlt]          = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+  colors[ImGuiCol_TextSelectedBg]         = ImVec4(1.00f, 0.37f, 0.17f, 0.25f);
+  colors[ImGuiCol_DragDropTarget]         = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_NavHighlight]           = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_NavWindowingHighlight]  = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+  colors[ImGuiCol_NavWindowingDimBg]      = ImVec4(0.00f, 0.00f, 0.00f, 0.59f);
+  colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(0.00f, 0.00f, 0.00f, 0.59f);
+
+
+	style->ChildRounding = 4.0f;
+	style->FrameBorderSize = 1.0f;
+	style->FrameRounding = 4.0f;
+	style->PopupRounding = 4.0f;
+	style->ScrollbarRounding = 12.0f;
+	style->WindowRounding = 4.0f;
+	style->GrabMinSize = 16.0f;
+	style->ScrollbarSize = 12.0f;
+	style->TabBorderSize = 0.0f;
+	style->TabRounding = 0.0f;
+	style->WindowPadding = ImVec2(6.0, 6.0);
+	style->FramePadding = ImVec2(10.5, 10.5);
+	style->CellPadding = ImVec2(10.5, 10.5);
+	style->ItemSpacing = ImVec2(10.0, 8.0);
+	style->ItemInnerSpacing = ImVec2(0.0, 6.0);
+  style->IndentSpacing = 12.0f;
+  style->WindowBorderSize = 1.0f;
+  style->ChildBorderSize = 1.0f;
+  style->PopupBorderSize = 1.0f;
+  style->GrabRounding = 4.0f;
+  style->WindowMenuButtonPosition = ImGuiDir_None;
+  style->WindowTitleAlign = ImVec2(0.5, 0.5);
 }
